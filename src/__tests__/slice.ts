a8a2ad1ff4c2e5cf8e41d6954ed4459881e395f4
Nic Waller <code@nicwaller.com>
2020-09-05 21:43:57 -0700

    add slicing code
    I sure do love string slicing in Python

---
+++ src/__tests__/slice.ts
@@ -0,0 +1,196 @@
+import { ConcreteSlice, linearSlices, Slice, slice, slicer } from '../slice';
+
+describe('positive offsets', () => {
+
+    test('gets whole string by default', () => {
+        const haystack = 'hello-world';
+        const needle = 'hello-world';
+        const selector: Slice = {};
+        const concrete: ConcreteSlice = slicer(haystack.length)(selector);
+        expect(concrete).toEqual({
+            start: 0,
+            end: 11,
+        })
+        expect(slice(haystack, concrete)).toEqual(needle);
+    });
+
+    test('gets whole string from start', () => {
+        const haystack = 'hello-world';
+        const needle = 'hello-world';
+        const selector: Slice = {start: 0};
+        const concrete: ConcreteSlice = slicer(haystack.length)(selector);
+        expect(concrete).toEqual({
+            start: 0,
+            end: 11,
+        })
+        expect(slice(haystack, concrete)).toEqual(needle);
+    });
+
+    test('skips first character', () => {
+        const haystack = 'hello-world';
+        const needle = 'ello-world';
+        const selector: Slice = {start: 1};
+        const concrete: ConcreteSlice = slicer(haystack.length)(selector);
+        expect(concrete).toEqual({
+            start: 1,
+            end: 11,
+        })
+        expect(slice(haystack, concrete)).toEqual(needle);
+    });
+
+    test('gets last character', () => {
+        const haystack = 'hello-world';
+        const needle = 'd';
+        const selector: Slice = {start: 10};
+        const concrete: ConcreteSlice = slicer(haystack.length)(selector);
+        expect(concrete).toEqual({
+            start: 10,
+            end: 11,
+        })
+        expect(slice(haystack, concrete)).toEqual(needle);
+    });
+
+    test('gets empty prefix', () => {
+        const haystack = 'hello-world';
+        const needle = '';
+        const selector: Slice = {start: 0, end: 0};
+        const concrete: ConcreteSlice = slicer(haystack.length)(selector);
+        expect(concrete).toEqual({
+            start: 0,
+            end: 0,
+        })
+        expect(slice(haystack, concrete)).toEqual(needle);
+    });
+
+    test('gets empty prefix, ambiguous start', () => {
+        const haystack = 'hello-world';
+        const needle = '';
+        const selector: Slice = {end: 0};
+        const concrete: ConcreteSlice = slicer(haystack.length)(selector);
+        expect(concrete).toEqual({
+            start: 0,
+            end: 0,
+        })
+        expect(slice(haystack, concrete)).toEqual(needle);
+    });
+
+    test('gets first character', () => {
+        const haystack = 'hello-world';
+        const needle = 'h';
+        const selector: Slice = {end: 1};
+        const concrete: ConcreteSlice = slicer(haystack.length)(selector);
+        expect(concrete).toEqual({
+            start: 0,
+            end: 1,
+        })
+        expect(slice(haystack, concrete)).toEqual(needle);
+    });
+
+    test('skips last character', () => {
+        const haystack = 'hello-world';
+        const needle = 'hello-worl';
+        const selector: Slice = {end: 10};
+        const concrete: ConcreteSlice = slicer(haystack.length)(selector);
+        expect(concrete).toEqual({
+            start: 0,
+            end: 10,
+        })
+        expect(slice(haystack, concrete)).toEqual(needle);
+    });
+});
+
+describe('negative offsets', () => {
+    test('gets last character', () => {
+        const haystack = 'hello-world';
+        const needle = 'd';
+        const selector: Slice = {start: -1};
+        const concrete: ConcreteSlice = slicer(haystack.length)(selector);
+        expect(concrete).toEqual({
+            start: 10,
+            end: 11,
+        })
+        expect(slice(haystack, concrete)).toEqual(needle);
+    });
+
+    test('skips last character', () => {
+        const haystack = 'hello-world';
+        const needle = 'hello-worl';
+        const selector: Slice = {end: -1};
+        const concrete: ConcreteSlice = slicer(haystack.length)(selector);
+        expect(concrete).toEqual({
+            start: 0,
+            end: 10,
+        })
+        expect(slice(haystack, concrete)).toEqual(needle);
+    });
+
+    test('gets second-last character', () => {
+        const haystack = 'hello-world';
+        const needle = 'l';
+        const selector: Slice = {start: -2, end: -1};
+        const concrete: ConcreteSlice = slicer(haystack.length)(selector);
+        expect(concrete).toEqual({
+            start: 9,
+            end: 10,
+        })
+        expect(slice(haystack, concrete)).toEqual(needle);
+    });
+});
+
+describe('linear', () => {
+    test('accepts adjacent slices', () => {
+        const haystack = 'hello-world';
+        const linear = linearSlices(haystack.length, [
+            {start: 0, end: 1},
+            {start: 1, end: 2},
+        ]);
+        expect(linear).toEqual([
+            {start: 0, end: 1},
+            {start: 1, end: 2},
+        ]);
+    });
+
+    test('reorders slices', () => {
+        const haystack = 'hello-world';
+        const linear = linearSlices(haystack.length, [
+            {start: 1, end: 2},
+            {start: 0, end: 1},
+        ]);
+        expect(linear).toEqual([
+            {start: 0, end: 1},
+            {start: 1, end: 2},
+        ]);
+    });
+
+    test('rejects repeated slice', () => {
+        const haystack = 'hello-world';
+        expect(() => {
+            const linear = linearSlices(haystack.length, [
+                {start: 0, end: 1},
+                {start: 0, end: 1},
+            ]);
+        }).toThrow();
+    });
+
+    test('rejects overlapping leading slices', () => {
+        const haystack = 'hello-world';
+        expect(() => {
+            const linear = linearSlices(haystack.length, [
+                {end: 4},
+                {end: 6},
+            ]);
+        }).toThrow();
+    });
+
+
+    test('rejects overlapping trailing slices', () => {
+        const haystack = 'hello-world';
+        expect(() => {
+            const linear = linearSlices(haystack.length, [
+                {start: 4},
+                {start: 6},
+            ]);
+        }).toThrow();
+    });
+
+});
---
+++ src/slice.ts
@@ -0,0 +1,69 @@
+// I like Python's idea of string slicing. Let's do more of that.
+// https://docs.python.org/3/tutorial/introduction.html#strings
+
+export interface Slice {
+    start?: number; // inclusive
+    end?: number; // exclusive
+}
+
+export interface ConcreteSlice {
+    start: number; // inclusive
+    end: number; // exclusive
+}
+
+export type SliceResolver = (slice: Slice) => ConcreteSlice;
+
+// TODO: it would be ideal to support Pythonic ranges like 1..-1
+export function slicer(size: number): SliceResolver {
+    return (slice: Slice) => {
+        function fx(x: number | undefined, def: number, scope: number) {
+            if (x === undefined) {
+                return def;
+            } else if (x < 0) {
+                return scope + x;
+            } else {
+                return x;
+            }
+        }
+        return {
+            start: fx(slice.start, 0, size),
+            end: fx(slice.end, size, size),
+        }
+    }
+}
+
+export function slice(input: string, slice: Slice) {
+    const s = slicer(input.length)(slice);
+    return input.slice(s.start, s.end);
+}
+
+function compareSlices(a: ConcreteSlice, b: ConcreteSlice) {
+    if (a.start < b.start) {
+        return -1;
+    } else if (a.start > b.start) {
+        return 1;
+    } else {
+        if (a.end < b.end) {
+            return -1;
+        } else if (a.end > b.end) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
+
+// Return an ordered list of non-overlapping slices
+export function linearSlices(size: number, slices: Slice[]): ConcreteSlice[] {
+    const resolved = slices.map(slicer(size))
+    const sorted = resolved.sort(compareSlices);
+    let cursor = 0;
+    for (const slice of sorted) {
+        if (slice.start < cursor || slice.end < cursor) {
+            throw Error(`found overlapping slice: ${slice.start}..${slice.end}`)
+        } else {
+            cursor = slice.end;
+        }
+    }
+    return sorted;
+}
